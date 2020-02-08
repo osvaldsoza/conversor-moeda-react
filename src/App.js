00a3d0e9deb5f7c52372c11edd00e700dfa10748
osvald.soza@gmail.com <osvald.soza@gmail.com>
2020-02-08 15:23:39 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Conversor from './components/Conversor';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App d-flex flex-column align-items-center">
-      <h2>Conversor de Moedas</h2>
-      <div className=" d-flex justify-content-sm-around">
-        <Conversor moedaA="USD" moedaB="BRL" />
-        <Conversor moedaA="BRL" moedaB="USD" />
-      </div>
-      <div className=" d-flex justify-content-sm-around">
-        <Conversor moedaA="CAD" moedaB="BRL" />
-        <Conversor moedaA="BRL" moedaB="CAD" />
-      </div>
-      <div className="d-flex justify-content-sm-around">
-        <Conversor moedaA="EUR" moedaB="BRL" />
-        <Conversor moedaA="BRL" moedaB="EUR" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="d-container d-flex flex-column align-items-center">
+            <h2>Conversor de Moedas</h2>
+            <div className="d-conversor">
+                <Conversor moedaA="USD" moedaB="BRL"/>
+                <Conversor moedaA="BRL" moedaB="USD"/>
+            </div>
+            <div className="d-conversor">
+                <Conversor moedaA="CAD" moedaB="BRL"/>
+                <Conversor moedaA="BRL" moedaB="CAD"/>
+            </div>
+            <div className="d-conversor">
+                <Conversor moedaA="EUR" moedaB="BRL"/>
+                <Conversor moedaA="BRL" moedaB="EUR"/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -1,36 +1,60 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 export default props => {
     const [moedaAValue, setMoedaAValue] = useState('0.00')
     const [moedaBValue, setMoedaBValue] = useState(0)
 
-    const converter = () => {
-      const de_para = `${props.moedaA}_${props.moedaB}`;
-      const uri = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=8760e82836b16b39ea54`;
-      fetch(uri)
-      .then(res =>{
-          return res.json();
-      })
-      .then(json =>{
-          const cotacao = json[de_para]
-          const moedaConvertida = (cotacao * parseFloat(moedaAValue)).toFixed(2)
-          setMoedaBValue(moedaConvertida)
-      });
+    const formatarMoeda = (moedaConvertida, cotacao) => {
+        switch (props.moedaB) {
+            case 'BRL':
+                moedaConvertida = (cotacao * parseFloat(moedaAValue))
+                    .toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+                return setMoedaBValue(moedaConvertida)
+            case 'USD':
+                moedaConvertida = (cotacao * parseFloat(moedaAValue))
+                    .toLocaleString('usd', {style: 'currency', currency: 'USD'})
+                return setMoedaBValue(moedaConvertida)
+            case 'CAD':
+                moedaConvertida = (cotacao * parseFloat(moedaAValue))
+                    .toLocaleString('usd', {style: 'currency', currency: 'usd'})
+                return setMoedaBValue(moedaConvertida)
+            case 'EUR':
+                moedaConvertida = (cotacao * parseFloat(moedaAValue))
+                    .toLocaleString('eur', {style: 'currency', currency: 'eur'})
+                return setMoedaBValue(moedaConvertida)
+            default:
+                return setMoedaBValue(moedaConvertida)
+        }
     }
 
-    const handleOnChange = (e)=>{
+    const converter = async () => {
+        const de_para = `${props.moedaA}_${props.moedaB}`;
+        const uri = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=8760e82836b16b39ea54`;
+        const dados = fetch(uri)
+            .then(res => {
+                return res.json();
+            })
+        await dados.then(json => {
+            const cotacao = json[de_para]
+            let moedaConvertida = 0.00
+            formatarMoeda(moedaConvertida, cotacao)
+        });
+    }
+
+    const handleOnChange = (e) => {
         setMoedaAValue(e.target.value)
     }
 
     return (
         <div className="conversor">
             <h2>{props.moedaA} to {props.moedaB}</h2>
-            <input type="text" 
-            onChange={handleOnChange}
-            value={moedaAValue}
+            <input type="text"
+                   onChange={handleOnChange}
+                   value={moedaAValue}
             />
             <button onClick={converter}>Converter</button>
             <h2>{moedaBValue}</h2>
         </div>
+
     )
 }
